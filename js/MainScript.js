55f30d1df4f8dae27946a3340bfd1415f711af80
Greesea <Greesea@vip.qq.com>
2015-03-18 21:23:22 +0800

    更改了类的声明方式(还没写注释 现在能成功的绘制出玩家所属颜色的20*20方块
    已知bug：
    20*20的正方形被拉伸成 40+ * 60+ 而且IE11下无法显示任何内容

--- js/MainScript.js
+++ js/MainScript.js
@@ -7,20 +7,25 @@ $(function () {
 
 function Initialize() {
     var mapObjArray = new Array();
-    mapObjArray[0] = MapObject.CreateNew(Location.CreateNew(50, 50), Size.CreateNew(20, 20), Region.Red, false, Draw);
-    mapObjArray[1] = MapObject.CreateNew(Location.CreateNew(100, 50), Size.CreateNew(20, 20), Region.Red, false, Draw);
-    mapObjArray[2] = MapObject.CreateNew(Location.CreateNew(150, 50), Size.CreateNew(20, 20), Region.Red, false, Draw);
-    mapObjArray[3] = MapObject.CreateNew(Location.CreateNew(200, 50), Size.CreateNew(20, 20), Region.Red, false, Draw);
-    mapObjArray[4] = MapObject.CreateNew(Location.CreateNew(250, 50), Size.CreateNew(20, 20), Region.Red, false, Draw);
+    //mapObjArray[0] = MapObject.CreateNew(Location.CreateNew(50, 50), Size.CreateNew(20, 20), Region.Red, false, Draw);
+    //mapObjArray[1] = MapObject.CreateNew(Location.CreateNew(100, 50), Size.CreateNew(20, 20), Region.Red, false, Draw);
+    //mapObjArray[2] = MapObject.CreateNew(Location.CreateNew(150, 50), Size.CreateNew(20, 20), Region.Red, false, Draw);
+    //mapObjArray[3] = MapObject.CreateNew(Location.CreateNew(200, 50), Size.CreateNew(20, 20), Region.Red, false, Draw);
+    //mapObjArray[4] = MapObject.CreateNew(Location.CreateNew(250, 50), Size.CreateNew(20, 20), Region.Red, false, Draw);
+
+    mapObjArray[0] = new MapObject(new Location(50, 50), new Size(20, 20), Region._Corner, false, Draw);
+    mapObjArray[1] = new MapObject(new Location(100, 50), new Size(20, 20), Region.Red, false, Draw);
+    mapObjArray[2] = new MapObject(new Location(150, 50), new Size(20, 20), Region.Blue, false, Draw);
+    mapObjArray[3] = new MapObject(new Location(200, 50), new Size(20, 20), Region.Orange, false, Draw);
+    mapObjArray[4] = new MapObject(new Location(250, 50), new Size(20, 20), Region.Yellow, false, Draw);
 
     gameArea = $("#gamearea")[0].getContext("2d");
-    var game = Game.Initialize(mapObjArray);
-    console.log(game.GetObjs());
+    //var game = Game.Initialize(mapObjArray);
+    var game = new Game(mapObjArray);
     game.GameStart();
 }
 
 function Draw() {
     gameArea.fillStyle = this.Region.toString();
     gameArea.fillRect(this.Location.X, this.Location.Y, this.Size.Width, this.Size.Height);
-    console.log("draw! " + this.Location.X + " " + this.Location.Y + " " + this.Size.Width + " " + this.Size.Height);
 }
